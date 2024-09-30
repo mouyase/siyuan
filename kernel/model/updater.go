b69118823df1c5305505e9a48f82ad7ba1497677
某亚瑟 <i@yojigen.tech>
2024-09-30 15:39:35 +0800

    feat: 修改更新地址

--- kernel/model/updater.go
+++ kernel/model/updater.go
@@ -133,21 +133,15 @@ func getUpdatePkg() (downloadPkgURLs []string, checksum string, err error) {
 			suffix = "mac.dmg"
 		}
 	}
-	pkg := "siyuan-" + ver + "-" + suffix
+	pkg := "siyuan-" + ver + "-bare-" + suffix
 
-	b3logURL := "https://release.b3log.org/siyuan/" + pkg
-	liuyunURL := "https://release.liuyun.io/siyuan/" + pkg
-	githubURL := "https://github.com/mouyase/siyuan/releases/download/v" + ver + "/" + pkg
+	githubURL := "https://github.com/mouyase/siyuan/releases/download/v" + ver + "-bare/" + pkg
 	ghproxyURL := "https://mirror.ghproxy.com/" + githubURL
 	if util.IsChinaCloud() {
-		downloadPkgURLs = append(downloadPkgURLs, b3logURL)
-		downloadPkgURLs = append(downloadPkgURLs, liuyunURL)
 		downloadPkgURLs = append(downloadPkgURLs, ghproxyURL)
 		downloadPkgURLs = append(downloadPkgURLs, githubURL)
 	} else {
 		downloadPkgURLs = append(downloadPkgURLs, githubURL)
-		downloadPkgURLs = append(downloadPkgURLs, b3logURL)
-		downloadPkgURLs = append(downloadPkgURLs, liuyunURL)
 	}
 
 	checksums := result["checksums"].(map[string]interface{})
